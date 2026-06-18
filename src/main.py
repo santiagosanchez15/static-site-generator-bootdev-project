e0ded7529fd0096fc10a5869777d92358e63d790
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-06-18 14:28:18 -0700

    small change to main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from generate_pages_r import generate_pages_recursive
 import sys
 
 def main():
-    basepath = sys.argv[1] if sys.argv[1] else "/"
+    basepath = sys.argv[1] if len(sys.argv) >=2 else "/"
     copy_static(source="static", destination="public")
     generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="public",basepath=basepath)
 
