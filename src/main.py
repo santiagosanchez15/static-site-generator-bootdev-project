b867e2912f6b74c80a9a08e64a3d904b2885c8f6
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-06-18 14:33:49 -0700

    few changes, public delete, not that you were suppose to see it before

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) >=2 else "/"
-    copy_static(source="static", destination="public")
-    generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="public",basepath=basepath)
+    copy_static(source="static", destination="docs")
+    generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="docs",basepath=basepath)
 
 main()
